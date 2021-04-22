657a1ae1277d05e999a14f6dbb778c22965e92d2
unknown <lordis.games@gmail.com>
2021-04-22 22:34:54 +0300

    cos

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("Я JavaScript!");
+let value = prompt("Введите число...")
+/*
+if(value >= 1){
+    alert("Ваше число больше нуля")
+}else if(value < 0){
+    alert("Ваше число меньше нуля")
+}else{
+    alert("Ваше число ноль")
+}
+*/
+
+let test = (value >= 1) ? alert("число больше 0") :  alert(" число меньше или равно 0");
